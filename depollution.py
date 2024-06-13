11c0c63903d2c1e2ed5f6e3413a90c8069d81a1a
marian <marian.szawelski@uha.fr>
2024-06-13 12:49:23 +0200

    maj finale depollution

--- depollution.py
+++ depollution.py
@@ -33,24 +33,39 @@ def scan_file_sophos(file_path):
     except Exception as e:
         print(f"Sophos: Une erreur s'est produite lors de l'analyse {file_path}: {str(e)}")
         return False
+    
 
-def scan_directory_clamav(directory_path):
+def scan_directory_clamav(directory_path, message_queue):
     infected_files = []
+    total_files = sum([len(files) for r, d, files in os.walk(directory_path)])
+    processed_files = 0
+    
     for root, dirs, files in os.walk(directory_path):
         for file in files:
             file_path = os.path.join(root, file)
             if not scan_file_clamav(file_path):
                 infected_files.append(file_path)
+            processed_files += 1
+            progress = (processed_files / total_files) * 50
+            message_queue.put(f"Progress: {progress:.2f}")
     return infected_files
 
-def scan_directory_sophos(directory_path):
+
+def scan_directory_sophos(directory_path, message_queue):
     infected_files = []
+    total_files = sum([len(files) for r, d, files in os.walk(directory_path)])
+    processed_files = 0
+    
     for root, dirs, files in os.walk(directory_path):
         for file in files:
             file_path = os.path.join(root, file)
             if not scan_file_sophos(file_path):
                 infected_files.append(file_path)
+            processed_files += 1
+            progress = 50 + (processed_files / total_files) * 50
+            message_queue.put(f"Progress: {progress:.2f}")
     return infected_files
+
 
 def scan_system_with_chkrootkit(message_queue):
     try:
@@ -121,12 +136,12 @@ def main_scan(message_queue):
         message_queue.put(f"Lancement de la première analyse antivirus.")
         message_queue.put(f"Première analyse en cours, veuillez patienter...")
         print(f"Analyse du dossier: {usb_mount_point}")
-        clamav_infected_files = scan_directory_clamav(usb_mount_point)
+        clamav_infected_files = scan_directory_clamav(usb_mount_point, message_queue)
         print(f"Première analyse terminée.")
         message_queue.put(f"Première analyse terminée.")
         message_queue.put(f"Lancement de la deuxième analyse antivirus.")
         message_queue.put(f"Deuxième analyse en cours, veuillez patienter...")
-        sophos_infected_files = scan_directory_sophos(usb_mount_point)
+        sophos_infected_files = scan_directory_sophos(usb_mount_point, message_queue)
         print(f"Deuxième analyse terminée.")
         message_queue.put(f"Deuxième analyse terminée.")
         all_infected_files = set(clamav_infected_files + sophos_infected_files)
@@ -142,9 +157,6 @@ def main_scan(message_queue):
             else:
                 print("Le système est infecté par un rootkit.")
                 message_queue.put("Le système est infecté par un rootkit.")
-            print("Relance de l'analyse complète...")
-            message_queue.put("Relance de l'analyse complète... \n")
-            main_scan(message_queue)
         else:
             print("Aucun fichier infecté trouvé.")
             message_queue.put("Aucun fichier infecté trouvé.")
@@ -153,6 +165,7 @@ def main_scan(message_queue):
         print(f"Le chemin fourni n'est ni un fichier ni un répertoire: {usb_mount_point}")
         sys.exit(1)
 
+        
 if __name__ == "__main__":
     message_queue = queue.Queue()
     main_scan(message_queue)
--- interface/depollutionResult.py
+++ interface/depollutionResult.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from tkinter import Button, Label, Toplevel, ttk
 import queue
 from PIL import Image, ImageTk
+import itertools
 
 class DepollutionResultView(tk.Frame):
     def __init__(self, master, message_queue):
@@ -8,25 +10,43 @@ class DepollutionResultView(tk.Frame):
         self.master = master
         self.message_queue = message_queue
         self.configure(bg="#2c3e50")
-        self.image_displayed = False  # Ajouter un flag pour vérifier si l'image est déjà affichée
+        self.image_displayed = False
+        self.animation_running = True
         self.create_widgets()
         self.update_messages()
+        self.animate_loading()  # Commencer l'animation au démarrage
 
     def create_widgets(self):
-        # Ajout d'un cadre pour contenir le champ de texte et centrer le bouton "Retour"
         frame = tk.Frame(self, bg="#2c3e50")
         frame.pack(pady=20, padx=20, fill=tk.BOTH, expand=True)
 
-        self.message_text = tk.Text(frame, font=("bitstream charter", 30), fg="white", bg="#2c3e50", wrap="word", state="disabled", height=10)
-        self.message_text.tag_configure("center", justify='center')
-        self.message_text.pack(pady=10, padx=10, fill=tk.BOTH, expand=True)
+        self.message_label = tk.Label(frame, text="\n Dépollution en cours, veuillez patienter...", font=("bitstream charter", 70), fg="white", bg="#2c3e50")
+        self.message_label.pack(pady=10, padx=10, )
 
-        self.image_label = tk.Label(self, bg="#2c3e50")  # Label pour afficher l'image
-        self.image_label.pack(pady=10)  # Ajout d'espace autour de l'image
+        # Frame pour la barre de progression, le pourcentage et l'animation
+        self.progress_frame = tk.Frame(self, bg="#2c3e50")
+        self.progress_frame.place(relx=0.5, rely=0.6, anchor=tk.CENTER)  # Centre la barre de progression et l'animation
+
+        self.animation_label = tk.Label(self.progress_frame, bg="#2c3e50")
+        self.animation_label.pack()
+
+        self.progress_label = tk.Label(self.progress_frame, text="0%", font=("bitstream charter", 40), fg="white", bg="#2c3e50")
+        self.progress_label.pack()
+
+        style = ttk.Style()
+        style.configure("custom.Horizontal.TProgressbar", foreground='#00D6E0', background='#00D6E0')
+
+        self.progress = ttk.Progressbar(self.progress_frame, style="custom.Horizontal.TProgressbar", orient="horizontal", length=1500, mode="determinate")
+        self.progress.pack(pady=10, ipady=20)  # ipady pour augmenter l'épaisseur de la barre
+        self.progress["value"] = 0
+        self.progress["maximum"] = 100
 
         back_button = tk.Button(self, text="Retour", font=("bitstream charter", 50), bg="#34495e", fg="#ecf0f1", bd=0, highlightthickness=0)
         back_button.pack(pady=20)
         back_button.config(command=self.go_back)
+
+        # Charger les images pour l'animation
+        self.load_animation_images()
 
     def go_back(self):
         from depollutionView import DepollutionView
@@ -35,20 +55,32 @@ class DepollutionResultView(tk.Frame):
     def update_messages(self):
         try:
             message = self.message_queue.get_nowait()
-            self.message_text.config(state="normal")
-            self.message_text.insert(tk.END, message + "\n", "center")
-            self.message_text.config(state="disabled")
-            self.message_text.see(tk.END)
-            
-            # Vérifie si le message indique qu'aucun fichier infecté n'a été trouvé
-            if "Aucun fichier infecté trouvé." in message:
-                self.display_image("images/check.png")  # Remplacez par le chemin de votre image
-                self.image_displayed = True  # Mettre à jour le flag
+            if "Progress:" in message:
+                progress = float(message.split(":")[1].strip())
+                self.progress["value"] = progress
+                self.progress_label.config(text=f"{int(progress)}%")
 
-            elif "Fichiers infectés trouvés" in message:
-                self.display_image("images/wrong2.png")
-                self.image_displayed = True
+               
+            elif "Aucun fichier infecté trouvé." in message:
+                self.animation_running = False  # Stopper l'animation
+                self.message_label.config(text="Dépollution terminée")
+                self.show_custom_popup("Information", "Aucun fichier infecté trouvé. \n Vous pouvez retirer votre clé.")
+                self.animation_label.destroy() # Supprimer l'animation et la barre de progression
                 
+            elif "Fichiers infectés trouvés et supprimés" in message:
+                self.animation_running = False
+                self.message_label.config(text="Dépollution terminée")
+                self.show_custom_popup("Alerte", "Fichiers infectés trouvés et\n supprimés. Le système est analysé. \n Une dépollution va être relancée.", fg="red")
+                self.message_label.config(text="Analyse du système en cours...")
+                self.animation_label.destroy()
+
+            elif "Le système est infecté par un rootkit." in message or "Aucun rootkit trouvé sur le système." in message:
+                self.animation_running = False
+                from depollutionView import DepollutionView
+                self.master.switch_frame(DepollutionView)
+                self.master._frame.start_depuration_process()
+                
+
         except queue.Empty:
             pass
 
@@ -56,7 +88,63 @@ class DepollutionResultView(tk.Frame):
 
     def display_image(self, image_path):
         image = Image.open(image_path)
-        image = image.resize((300, 300), Image.LANCZOS)  # Redimensionner l'image si nécessaire
+        image = image.resize((300, 300), Image.LANCZOS)
         photo = ImageTk.PhotoImage(image)
         self.image_label.config(image=photo)
-        self.image_label.image = photo  # Pour éviter que l'image soit collectée par le garbage collector
+        self.image_label.image = photo
+
+    def load_animation_images(self):
+        self.animation_images = []
+        for i in range(1, 31): 
+            image_path = f"images/loading/{i}.png"
+            image = Image.open(image_path)
+            image = image.resize((300, 300), Image.LANCZOS)
+            self.animation_images.append(ImageTk.PhotoImage(image))
+        self.animation_cycle = itertools.cycle(self.animation_images)
+        
+    def animate_loading(self):
+        if self.animation_running:
+            self.animation_label.config(image=next(self.animation_cycle))
+            self.after(25, self.animate_loading)  # Changer d'image toutes les 100 ms
+
+    def show_custom_popup(self, title, message, fg="black"):
+        popup = Toplevel(self)
+        popup.title(title)
+
+        # Get the screen width and height
+        screen_width = popup.winfo_screenwidth()
+        screen_height = popup.winfo_screenheight()
+
+        # Calculate position
+        x = (screen_width // 4) - (1200 // 2)
+        y = (screen_height // 2) - (700 // 2)
+
+        # Set the position of the window to the center of the screen
+        popup.geometry('%dx%d+%d+%d' % (1200, 700, x, y))  # Augmenter la hauteur pour l'image
+
+        label = Label(popup, text=message, font=("bitstream charter", 50), fg=fg)
+        label.pack(pady=20)
+
+        if "Aucun fichier infecté trouvé." in message:
+            image_path = "images/check.png"
+            image = Image.open(image_path)
+            image = image.resize((250, 250), Image.LANCZOS)  # Redimensionner l'image si nécessaire
+            photo = ImageTk.PhotoImage(image)
+            image_label = Label(popup, image=photo)
+            image_label.image = photo  # Garder une référence à l'image pour éviter qu'elle soit garbage collected
+            image_label.pack(pady=10)
+        
+        else:
+            image_path = "images/wrong2.png"
+            image = Image.open(image_path)
+            image = image.resize((150, 150), Image.LANCZOS)  # Redimensionner l'image si nécessaire
+            photo = ImageTk.PhotoImage(image)
+            image_label = Label(popup, image=photo)
+            image_label.image = photo  # Garder une référence à l'image pour éviter qu'elle soit garbage collected
+            image_label.pack(pady=10)
+
+
+        
+
+        ok_button = Button(popup, text="OK", font=("bitstream charter", 50), fg="black", command=popup.destroy)
+        ok_button.pack(pady=20)
